a759e7d018ff14a762e9deddc4e522ad8d7ee1ac
IsidoraGuinez <isiguinez@uc.cl>
2025-11-27 13:04:29 -0300

    add arduino

---
+++ config.js
@@ -0,0 +1,15 @@
+Protobject.setProduction(true)
+Protobject.initialize(
+	[
+		{ 
+			name: "Servo",
+			page: "arduino.html",
+		},
+		{ 
+			name: "OlaCalor",
+			page: "index.html",
+			main: true,
+		}
+	]);
+
+ 
